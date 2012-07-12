17163c5e85958eb8c1dfaeea50e49cf0d14570c4
Mike Hoye <mhoye@dojo.(none)>
2012-07-11 22:43:57 -0400

    More development...i

--- xterwm.c
+++ xterwm.c
@@ -1,7 +1,7 @@
 /* XterWM was written by Mike Hoye <mhoye@bespokeio.com> in mid-2012 out of pure spite.
  *
  * XterWM is intended to be opened as a secondary X session in addition to your WM of
- * choice. The sole design goals for XterWM were correctly-sized fullscreen terminals
+ * choice. The sole design goals for XterWM were correctly-sized terminals
  * and not going completely insane 
  * 
  *
@@ -13,6 +13,13 @@
 #include <X11/Xlib.h>
 #include <X11/extensions/Xinerama.h>
 #include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <syslog.h>
+
+#define XTERM_BINARY "/usr/bin/xterm"
+#define ASSUMPTION_ERROR -100 
+#define FORK_ERROR        -101
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MAXSCREENS 16
@@ -23,7 +30,7 @@ int main(void)
     Display * dpy;
     XWindowAttributes attr;
     XineramaScreenInfo * xinf;
-      
+    pid_t xterm_pids[MAXITEMS];  
     int screens;
     XButtonEvent start;
     XEvent ev;
@@ -35,7 +42,7 @@ int main(void)
 
     /* Now, find out: How many actual physical monitors do we have here? */
     
-    xinf = XineramaQueryScreens(dpy, &screens);
+    xinf = XineramaQueryScreens(dpy, * screens);
 
     XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F1")), Mod1Mask,
             DefaultRootWindow(dpy), True, GrabModeAsync, GrabModeAsync);
@@ -50,7 +57,9 @@ int main(void)
         XNextEvent(dpy, &ev);
 
 	/* here's where it goes - if you add a new monitor, manage it, if you remove it, kill whatever was shown in it.*/
-
+        
+        if(ev.type == XRRScreenEventNotify)
+          frobAllTheTerms(dpy,  xinf, screens, xterm_pids);
 
         if(ev.type == KeyPress && ev.xkey.subwindow != None)
             XRaiseWindow(dpy, ev.xkey.subwindow);
@@ -76,15 +85,77 @@ int main(void)
 
         else if(ev.type == XRRScreenChangeNotifyEvent)
         {
-            if(0 != frobAllTheTerms(dpy, xinf, screens)) return 3;
+            if(0 != frobAllTheTerms(dpy, screens)) return 3;
 
         }
 
     }
 }
 
-int frobAllTheTerms(Display * dpy, XineramaScreenInfo * xinf, int prev_screens)
+int frobAllTheTerms(Display * dpy, int * prevscr, pid_t * xterm_pids)
 {
-   /* This is pretty straigtforward. The program has a list of xterms that are running, 
-    * and a list of phyusical screens it extracts from Xinerama. 
+   /* This is pretty straightforward. The program has a list of xterms that are running, 
+    * and a list of phyusical screens it extracts from Xinerama. When monitors are attached 
+    * add an appropriately-sized Xterm to each new monitor. The part you may like much less is
+    * this: when you _de_tach a monitor, walk the list of xterminals (without even bothering to
+    * figure out which one was on what monitor, resize the first N terminals it finds in its
+    * list to fit the N remaining monitors and kills the rest, regardless of what they're doing.
+    *
+    * This behavior is not a bug; it's an explicit design decision, and if it's a problem for you,
+    * I suggest you use screen, or dtach, or somebody else's window manaand kills the rest, regardless of what they're doing.
+    *
+    * This behavior is not a bug; it's an explicit design decision, and if it's a problem for you,
+    * I suggest you use screen, or dtach, or somebody else's window manager. ger. 
+    * 
+    */
+    int curscr, c, r, pid;
+    char* xterm_opts[64]; /* This program will fail if we ever have sixteen-digit screen
+                           resolutions, but if you're still using this program when pixel 
+                           densities get to that point, um, hey bro wtf r u doin */
+
+    XineramaScreenInfo * xsi = XineramaScreenInfo(dpy, * curscr); 
+
+    // pseudocode: if screen numbers are equal, od nothing, if more
+    current screens than pids, walk up and kill them, if smaller create //
+    your list of new ones.
+
+    if ( curscr == prevscr) {
+      /* I'm assuming that "receiving an XRRScreenChangeNotifyEvent"
+      is an atomic
+       * event - that is, it could be a screen being added, moved or
+       rotated, but * it is never more than one of those things at
+       once. If I'm wrong about that * this program is horribly broken
+       and you shouldn't use it.  * * We do nothing in this case, because
+       we're going to resize all the screens * after all the options as
+       a last step before leaving this function.  * */
+    } else if ( curscr > prevscr) {
+        if ( xterm_pids[curscr -1 ] > 1 ) {
+          kill( xterm_pids[curscr - 1], 9) // Children shouldn't play
+          with guns
+        } else {
+          syslog(LOG_USER, "Tried to kill the wrong process,
+          exiting...") && return(ASSUMPTION_ERROR);
+        } xterm_pids[curscr -1] = -1;
+      }
+    else if ( current screens > prevscr ) {
+        /* Figure out where the new screen is, then fork an xTerminal
+        out to it. */ 
+        pid = fork();
+        if (-1 == pid ) { 
+            syslog(LOG_USER, "Could not fork. What's up with that? I'm out.");
+
+            return(ASSUMPTION_ERROR); }
+        else if (pid = 0) 
+        { //child process.....
+            xterm_opts = strcat ( "-geometry ", 
+                                itoa(xsi[curscr-1].width), "+", 
+                                itoa(xsi[curscr-1].height), "+",  
+                                itoa(xsi[curscr-1].x_org), "+", 
+                                itoa(xsi[curscr-1].y_org) );
+            execv(XTERM_BINARY, xterm_opts);
+            return(0);
+            }
+
+
+        }
 
